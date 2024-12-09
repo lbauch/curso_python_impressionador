c10f6e88f598d542c38c75a3d1417f90735f71a9
lbauch <bauchspiess.lucas@gmail.com>
2024-12-09 12:34:36 -0300

    functions - pt6

--- auxiliares/funcoes_importantes.py
+++ auxiliares/funcoes_importantes.py
@@ -616,15 +616,14 @@ print(minha_soma(10, 13, 1, 10, 90, 0, 9, 8))
 
 def minha_soma(arg1, arg2, *args1, arg3, arg4, **args2):
     soma = 0
-    soma += arg1 - arg2
+    soma += arg1 + arg2
     print(soma)
     for arg in args1:
         soma += arg
     print(soma)
     soma += arg3 + arg4
     print(soma)
-    for args in args2:
-        print(args)
-
+    for key,value in args2.items():
+        print(value)
 # Atribui quantos parâmetros forem necessários.
-print(minha_soma(10, 5, 1, 10, arg1 = 90, arg2 =  0, arg3 = 9, arg4 = 8))
+print(minha_soma(10, 5, 1, 10, arg3 = 90, arg4 =  3, arg5 = 9, arg6 = 8))
